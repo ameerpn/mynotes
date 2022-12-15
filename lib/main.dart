69deeec5d0d017ab22b221e8cf64c5b5e9a8b868
ameerpn <ameerpn@hotmail.com>
2022-12-15 07:50:19 +0300

    step-11

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Notes',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
